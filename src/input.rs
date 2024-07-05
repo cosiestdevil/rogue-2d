1d89e139ef2e046790c6ef606d1190993860f064
Justin Wiblin <justinw@cosiestdevil.uk>
2024-07-05 11:06:44 +0100

    Refactor player movement and animation logic
    The player movement function has been refactored to simplify the code. The angle calculation now includes a modulo operation for better handling of edge cases. The animation selection process has been streamlined by removing redundant checks and using range containment instead. This results in cleaner, more readable code with the same functionality as before.

--- src/input.rs
+++ src/input.rs
@@ -44,54 +44,43 @@ fn player_movement(
         transform.translation.x += movement_direction.x * time.delta_seconds() * SPEED;
         transform.translation.y += movement_direction.y * time.delta_seconds() * SPEED;
         //let angle = Vec2::X.dot(player.facing).acos().to_degrees();
-        let angle = player.facing;
+        let angle = (player.facing + 180.0) % 360.0;
         let mut player_entity = commands.entity(entity);
-
         let moving = movement_direction.length() > 0.0;
-        let mut animation = *player
-            .animations
-            .get(if moving {
+        let animation = if (45.0..135.0).contains(&angle) {
+            if moving {
+                &PlayerAnimation::WalkLeft
+            } else {
+                &PlayerAnimation::IdleLeft
+            }
+        } else if (135.0..225.0).contains(&angle) {
+            if moving {
                 &PlayerAnimation::WalkDown
             } else {
                 &PlayerAnimation::IdleDown
-            })
-            .unwrap();
-        if (45.0..135.0).contains(&angle) {
-            animation = *player
-                .animations
-                .get(if moving {
-                    &PlayerAnimation::WalkRight
-                } else {
-                    &PlayerAnimation::IdleRight
-                })
-                .unwrap();
-        }
-        if !(-135.0..=135.0).contains(&angle) {
-            animation = *player
-                .animations
-                .get(if moving {
-                    &PlayerAnimation::WalkUp
-                } else {
-                    &PlayerAnimation::IdleUp
-                })
-                .unwrap();
-        }
-        if (-135.0..-45.0).contains(&angle) {
-            animation = *player
-                .animations
-                .get(if moving {
-                    &PlayerAnimation::WalkLeft
-                } else {
-                    &PlayerAnimation::IdleLeft
-                })
-                .unwrap();
-        }
+            }
+        } else if (225.0..315.0).contains(&angle) {
+            if moving {
+                &PlayerAnimation::WalkRight
+            } else {
+                &PlayerAnimation::IdleRight
+            }
+        } else if moving {
+            &PlayerAnimation::WalkUp
+        } else {
+            &PlayerAnimation::IdleUp
+        };
 
-        player_entity.insert(SpritesheetAnimation::from_id(animation));
+        player_entity.insert(SpritesheetAnimation::from_id(
+            *player.animations.get(animation).unwrap(),
+        ));
     }
 }
 
-fn player_rotate(bindings: Res<Ineffable>, /*time: Res<Time>,*/ mut query: Query<&mut Player>) {
+fn player_rotate(
+    bindings: Res<Ineffable>,
+    /*time: Res<Time>,*/ mut query: Query<&mut Player>,
+) {
     if let Ok(mut player) = query.get_single_mut() {
         let mut direction = bindings.direction_2d(ineff!(PlayerInput::Face));
 
